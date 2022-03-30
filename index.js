6f9b3ab0bb8e777e3969cf041356bc79104e1f24
siweh <sanemadesi@gmail.com>
2022-03-31 00:37:04 +0200

    Managed to fix displaying the filtered cars for the second part

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let colorsElem = document.querySelector('.listOfColors');
 let carColor = document.querySelector('.carColor');
 let carBrand = document.querySelector('.carBrand');
 let analyzeBtn = document.querySelector('.analyze');
+let showFilteredResults = document.querySelector('.filteredResults');
 let hbsDisplay = document.querySelector('.apiList').innerHTML;
 
 var theTemplate = Handlebars.compile(hbsDisplay);
@@ -36,13 +37,31 @@ async function getListOfColors(){
     })
 }
 
-function filterByColorAndBrand() {
-    let results = carColor;
-    console.log(results);
+async function filterByColorAndBrand() {
+   let color = carColor.value;
+   console.log(color);
+   let make = carBrand.value;
+   console.log(make);
+   let cars = await axios.get("http://api-tutor.herokuapp.com/v1/cars");
+    console.log(cars.data);
+//    let results = cars.data.map(car => {
+//        if(car === colors && car === brand){
+//          return car;
+//        }
+//    })
+   let filteredCarsByColorAndBrand = cars.data.filter((car) => {
+        return car.color === color && car.make === make;
+   });
+   console.log(filteredCarsByColorAndBrand);
+   showFilteredResults.innerHTML = theTemplate({
+       filteredCars: filteredCarsByColorAndBrand
+   });
+   return filteredCarsByColorAndBrand
 }
 
 getAllCars();
 getAllMakeCars();
 getListOfColors();
+//await filterByColorAndBrand();
 
 analyzeBtn.addEventListener('click', filterByColorAndBrand)
